f233bd24f50373949091826255f1b0fb30d6ce9b
rafaelvalquer <rafael.valquer@gmail.com>
2024-07-28 14:00:45 -0300

    first commit

--- Backend/config/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    uri: "mongodb+srv://rafaelvalquer:mudar123.@cluster.rysnuzj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster'",
-    options: {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-};
