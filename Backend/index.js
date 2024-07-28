5db4631444f74021f1e63bfbecdc7cae3d100446
rafaelvalquer <rafael.valquer@gmail.com>
2024-07-28 14:02:32 -0300

    primeira versão

---
+++ Backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+const dbConfig = require("./config/db");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Conectar ao MongoDB
+mongoose.connect(dbConfig.uri, dbConfig.options)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log("MongoDB connection error:", err));
+
+// Usar rotas
+app.use("/api/users", userRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
